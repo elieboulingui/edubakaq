727ccd6dfbff7fb14fc4e04fb17861b4537ad722
elieboulingui <124277238+elieboulingui@users.noreply.github.com>
2026-08-02 03:31:57 +0100

    Create robots.ts

---
+++ app/robots.ts
@@ -0,0 +1,11 @@
+import { MetadataRoute } from "next";
+
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: {
+      userAgent: "*",
+      allow: "/",
+    },
+    sitemap: "https://edubakaq-ashy.vercel.app/sitemap.xml",
+  };
+}
