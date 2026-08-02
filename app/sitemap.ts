4158a0b5ef51eb6ac1859dac864435d5ac432e26
elieboulingui <124277238+elieboulingui@users.noreply.github.com>
2026-08-02 03:22:22 +0100

    Update sitemap.ts

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,1 +1,11 @@
 
+import { MetadataRoute } from "next";
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  return [
+    {
+      url: "https://edubakaq-ashy.vercel.app",
+      lastModified: new Date(),
+    },
+  ];
+}
